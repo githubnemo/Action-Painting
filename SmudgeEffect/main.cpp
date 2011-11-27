def5af87903c019ecabc62d27d42e133f381f208
Lucas Jenss <lucas@x3ro.de>
2011-11-27 20:47:50 +0100

    *rupf* *rupf*

--- SmudgeEffect/main.cpp
+++ SmudgeEffect/main.cpp
@@ -36,8 +36,6 @@ int brushDistance = 1;      // Defines how often we draw the brush, in this case
 int smudgeBufferSize = 4;
 
 // Algorithm specific variables
-//int smudgeBufferSize = 0;
-//IplImage** smudgeBuffer;
 std::vector<IplImage*> smudgeBuffer(0);
 IplImage* brushMask;
 int lastX;
@@ -80,12 +78,7 @@ IplImage* cvCopySubImage(IplImage* src, int x, int y, int width, int height)
     return dst;
 }
 
-void drawImageAlphaMask(const IplImage* src, const IplImage* dst, const IplImage* mask) {
-    assert(src->width == mask->width && src->height == mask->height);
 
-
-
-}
 
 IplImage* createBrushMask(int brushRadius, int brushMaskBorder)
 {
@@ -106,166 +99,22 @@ IplImage* createBrushMask(int brushRadius, int brushMaskBorder)
 
     cvSetImageROI(brushMask, cvRect(brushMaskBorder, brushMaskBorder, brushRadius*2, brushRadius*2));
 
-    cvShowImage("test", brushMask);
+    cvShowImage("BrushMask", brushMask);
 
     return brushMask;
 }
 
-CvScalar getValueForPoint(int x, int y, int xi, int yi) {
-    //IplImage* src = smudgeBuffer.at(0);
-    
-    double r = 0;
-    double g = 0;
-    double b = 0;
-    
-    double percentages[] = {0.5, 0.25, 0.15, 0.1};
-    
-    unsigned long size = MIN(smudgeBufferSize, smudgeBuffer.size());
-    for(int i=0; i < size; i++) {
-        IplImage * current = smudgeBuffer.at(i);
-        CvScalar point = cvGet2D(current, yi, xi);
-        
-        
-        
-        
-        b += point.val[0] * percentages[i];
-        g += point.val[1] * percentages[i];
-        r += point.val[2] * percentages[i];
-        
-        //printf("i=%d -> in_r: %f in_g: %f in_b: %f\n", i, r, g, b);
-    }
-    
-    //printf("r: %f g: %f b: %f\n", r, g, b);
-    
-
-    
-    return cvScalar(b, g, r);
-}
 
 
-void drawSingleSmudgeStep(int x, int y) {
-    IplImage* src = smudgeBuffer.at(0);
-    
-    
-    // The mouse cursor is the center of our brush, but we need (x, y) to
-    // be the upper right corner of our brush.
-    x = x - (src->width/2);
-    y = y - (src->width/2);
-    
-    
-    // Make sure we don't run out of bounds. Begin.
-    if(x < 0) {
-        x = 0;
-    }
-    
-    if(y < 0) {
-        y = 0;
-    }
-    
-    if(x > (img->width - src->width)) {
-        x = img->width - src->width;
-    }
-    
-    if(y > (img->height - src->height)) {
-        y = img->height - src->height;
-    }
-    // Make sure we don't run out of bounds. End.
-    
-    
-    
-    if(abs(x - lastX) < brushDistance && abs(y - lastY) < brushDistance) {
-        return;
-    }
-    
-    lastX = x;
-    lastY = y;
-    
-    //cvSetImageROI(img, cvRect(x, y, src->width, src->height));
-    //cvAddWeighted(img, 0.5, src, 0.5, 0, img);
-    //cvResetImageROI(img);
-    
-    for(int yi=0; yi < src->height && y+yi < img->height;  yi++) {
-        for(int xi=0; xi < src->width && x+xi < img->width; xi++) {
-            
-            
-            CvScalar source = cvGet2D(img, y+yi, x+xi);
-            //CvScalar value = cvGet2D(src, yi, xi);
-            CvScalar value = getValueForPoint(x, y, xi, yi);
-            CvScalar mask = cvGet2D(brushMask, yi, xi);
-            
-            double nalpha = mask.val[0] / 255;
-            double alpha = 1 - nalpha;
-            value.val[0] = source.val[0] * nalpha + value.val[0] * alpha;
-            value.val[1] = source.val[1] * nalpha + value.val[1] * alpha;
-            value.val[2] = source.val[2] * nalpha + value.val[2] * alpha;
-            
-            cvSet2D(img, (y+yi), (x+xi), value);
-            
-        }
-    }
-
-}
-
-void applySmudge(int x, int y)
-{
-    if(!dragging) return;
-
-    while(smudgeBuffer.size() > smudgeBufferSize) {
-        smudgeBuffer.erase(smudgeBuffer.begin());
-    }
-    
-    IplImage* brush = cvCopySubImage(img, x-brushRadius, y-brushRadius, brushRadius*2, brushRadius*2);
-    smudgeBuffer.push_back(brush);
-    
-    if(smudgeBuffer.size() == 1) {
-        lastX = x;
-        lastY = y;
-
-        brushMask = createBrushMask(brushRadius, brushMaskBorder);
-        //cvReleaseImage(&brushMask);
-    } else {
-
-        int deltaX = x - lastX;
-        int deltaY = y - lastY;
-        
-        if(deltaX > 0) {
-            // y = slope*x + b
-            double slope = (double) deltaY / (double) deltaX;
-            double b = y - (x * slope);
-            
-            int cX = lastX;
-            while(cX < x) {
-                cX++;
-                double cY = cX * slope + b;
-                drawSingleSmudgeStep(cX, cY);
-                
-                if(debug)
-                    printf("--> cX: %d, cY: %f slope: %f \n", cX, cY, slope);
-            }
-            
-            
-        }
-        
-        printf("x: %d, y: %d \n", x, y);
-        drawSingleSmudgeStep(x, y);
-        
-        //cvSetImageROI(img, cvRect(x, x, brushRadius, brushRadius));
-        
-    }
-    
-    
-}
 
 void onMouse( int event, int x, int y, int flags, void* param )
 {
-    applySmudge(x, y);
 	drawCursor(x,y);
 
 	if(event==CV_EVENT_LBUTTONDOWN) {
         dragging = TRUE;
     } else if(event==CV_EVENT_LBUTTONUP) {
         dragging = FALSE;
-        smudgeBuffer.clear();
     }
 
 }
@@ -276,9 +125,7 @@ int main (int argc, const char* argv[])
     const char* windowName = "Smudge Demo";
 
     cvNamedWindow(windowName, CV_WINDOW_AUTOSIZE);
-    cvNamedWindow("test", CV_WINDOW_AUTOSIZE);
-
-
+    cvNamedWindow("BrushMask", CV_WINDOW_AUTOSIZE);
 
     // Some sliders to modify the algorithm values
     cvCreateTrackbar("Brush Size", windowName, &brushRadius, 20);
@@ -287,31 +134,13 @@ int main (int argc, const char* argv[])
     // Create our initial image, which we will smudge
     img = cvCreateImage(cvSize(640, 480), 8, 3);
     cvSet(img, CV_RGB(255, 255, 255));
-    cvRectangle(img, cvPoint(150, 150), cvPoint(300, 300), cvScalar(0), CV_FILLED);
+    cvRectangle(img, cvPoint(150, 150), cvPoint(300, 300), CV_RGB(0, 0, 255), CV_FILLED);
 
     // The screen buffer is whats actually displayed to the user, containing,
     // for example, the cursor position.
     screenBuffer = cvCloneImage(img);
 
-    cvSetMouseCallback(windowName,&onMouse, 0 );
-
-
-    /* Begin: Simulate some smudging */
-    dragging = TRUE;
-    applySmudge(285, 195);
-    applySmudge(296, 191);
-    debug = TRUE;
-    applySmudge(349, 174);
-    debug = FALSE;
-    applySmudge(385, 163);
-    applySmudge(445, 149);
-    applySmudge(454, 146);
-    applySmudge(454, 146);
-    dragging = FALSE;
-    smudgeBuffer.clear();
-    screenBuffer = cvCloneImage(img);
-    /* End: Simulate some smudging */
-    
+    cvSetMouseCallback(windowName, &onMouse, 0);
     
     // Re-render our image ~ every 10ms
     while(TRUE) {
