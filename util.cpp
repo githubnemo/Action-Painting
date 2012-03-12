d89b7c8e1090d74333537f3bd5b9cffafa4a4e2e
nemo <nemo@nemo.(none)>
2012-03-12 17:43:46 +0100

    Forgot to add util.* source files

---
+++ util.cpp
@@ -0,0 +1,23 @@
+#include "util.h"
+
+#ifdef USE_GLUT
+#if (XN_PLATFORM == XN_PLATFORM_MACOSX)
+        #include <GLUT/glut.h>
+#else
+        #include <GL/glut.h>
+#endif
+#else
+#include "opengles.h"
+#endif
+
+
+
+void glPrintString(void *font, char *str)
+{
+	size_t i,l = strlen(str);
+
+	for(i=0; i<l; i++)
+	{
+		glutBitmapCharacter(font,*str++);
+	}
+}
---
+++ util.h
@@ -0,0 +1,8 @@
+#ifndef __UTIL_H__
+#define __UTIL_H__
+
+#include <XnCppWrapper.h>
+
+void glPrintString(void* font, char* str);
+
+#endif
